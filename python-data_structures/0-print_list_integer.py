724226f0a5081e48d7c31d9cf821f94011f99ce8
Ntand42 <ntandom2003@gmail.com>
2025-07-13 21:21:22 +0000

    Task 0: Print all integers in a list using str.format()

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 #!/usr/bin/python3
+=======
+>>>>>>> 244cec7 (Task 0: Print all integers in a list using str.format())
 def print_list_integer(my_list=[]):
     for i in my_list:
         print("{:d}".format(i))
