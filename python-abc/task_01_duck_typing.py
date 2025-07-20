9fbcfea1ef77d74cadf014193f97710c3fe11296
Ntand42 <ntandom2003@gmail.com>
2025-07-20 17:35:30 +0000

    feat: Implement Shape ABC and Circle, Rectangle with shape_info function

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,20 @@
+ def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(shape):
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
+
+
+
